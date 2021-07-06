633845ad1271c2fa36155b402d2de95ff4d456e9
TroJIJI <adisrahimov0301@gmail.com>
2021-07-06 13:54:20 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw312/Adapter.java
@@ -0,0 +1,53 @@
+package com.example.hw312;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.viewHolder> {
+    private TextView name, surname;
+    private ArrayList<Students> list = new ArrayList<>();
+    public void addItem(Students students){
+
+        list.add(students);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.viewHolder holder, int position) {
+        holder.onBind(list.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            surname = itemView.findViewById(R.id.surname);
+        }
+
+        public void onBind(Students students) {
+            name.setText(students.getName());
+            surname.setText(students.getSurname());
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/hw312/OnButtonClickListener.java
@@ -0,0 +1,5 @@
+package com.example.hw312;
+
+public interface OnButtonClickListener {
+    void onClick();
+}
